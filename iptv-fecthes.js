fbeb25d2c2cb24f95a77ac50bd69f784d0f76a94
Arman Hossain <priyoarman1996@gmail.com>
2026-07-03 19:50:26 +0200

    commit

--- iptv-fecthes.js
+++ iptv-fecthes.js
@@ -1,8 +1,8 @@
-const baseUrl = "http://s.rocketdns.info:8080/live/monstercable/Dq6jjknxCr/";
+const baseUrl = "http://91.208.115.26:80/play/live.php?mac=00:1B:79:44:18:49&stream=";
 let links = "";
 
-for (let i = 2400; i <= 2700; i++) {
-    links += `${baseUrl}${i}.ts\n`;
+for (let i = 1745080; i <= 1745124; i++) {
+    links += `${baseUrl}${i}.&extension=m3u8\n`;
 }
 
 // Option 1: Log to console
